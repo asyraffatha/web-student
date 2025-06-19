4f210ba67255384e6ebe9b90970147b46df6f860
asyraffatha <asyraffrizqy07@gmail.com>
2025-06-19 23:03:14 +0700

    update_calendar

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,24 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EventController extends Controller
 {
     public function index()
     {
-        return Event::all();
+    return Event::where('user_id', Auth::id())->get();
     }
-
+    
     public function store(Request $request)
-    {
-        $event = Event::create($request->all());
-        return response()->json($event, 201);
+    {   
+    $data = $request->all();
+    $data['user_id'] = Auth::id(); // Gunakan Auth::id()
+    $event = Event::create($data);
+    return response()->json($event, 201);
     }
 
     public function destroy($id)
     {
-    $event = Event::findOrFail($id);
+    $event = Event::where('id', $id)
+                 ->where('user_id', Auth::id())
+                 ->firstOrFail();
     $event->delete();
-    return response()->json(null, 204); // Mengembalikan status 204 No Content
+    return response()->json(null, 204);
     }
 }
