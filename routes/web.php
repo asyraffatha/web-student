195de1b5642de66a085fb65ca8915187212913bb
asyraffatha <asyraffrizqy07@gmail.com>
2025-07-15 01:51:03 +0700

    FixTestingAuth_15juli

--- routes/web.php
+++ routes/web.php
@@ -158,9 +158,9 @@ Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
 
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
+// Route::get('/register', function () {
+//     return view('auth.register');
+// })->name('register');
 
 // Rute Profile (Hanya Bisa Diakses oleh Pengguna yang Sudah Login)
 Route::middleware(['auth', 'role:siswa'])->group(function () {
--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -9,7 +9,7 @@ test('login screen can be rendered', function () {
 });
 
 test('users can authenticate using the login screen', function () {
-    $user = User::factory()->create();
+    $user = User::factory()->create(['role' => 'siswa']);
 
     $response = $this->post('/login', [
         'email' => $user->email,
@@ -17,11 +17,11 @@ test('users can authenticate using the login screen', function () {
     ]);
 
     $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
+    $response->assertRedirect(route('home', absolute: false));
 });
 
 test('users can not authenticate with invalid password', function () {
-    $user = User::factory()->create();
+    $user = User::factory()->create(['role' => 'siswa']);
 
     $this->post('/login', [
         'email' => $user->email,
@@ -32,10 +32,10 @@ test('users can not authenticate with invalid password', function () {
 });
 
 test('users can logout', function () {
-    $user = User::factory()->create();
+    $user = User::factory()->create(['role' => 'siswa']);
 
     $response = $this->actingAs($user)->post('/logout');
 
     $this->assertGuest();
-    $response->assertRedirect('/');
+    $response->assertRedirect('/login');
 });
